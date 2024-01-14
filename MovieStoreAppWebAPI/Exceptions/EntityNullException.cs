54f402f4f87d804e1d5b302b0762840557c98bab
muhammet-enes-aksoy <muhammed_enes43@hotmail.com>
2024-01-14 23:36:05 +0300

    Created constants

---
+++ MovieStoreAppWebAPI/Exceptions/EntityNullException.cs
@@ -0,0 +1,9 @@
+﻿namespace MovieStoreAppWebAPI.Exceptions
+{
+    public class EntityNullException : Exception
+    {
+        public EntityNullException(string? message) : base(message)
+        {
+        }
+    }
+}
